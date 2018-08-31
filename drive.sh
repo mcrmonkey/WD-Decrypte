332bb5985147e1664bff71440db9197e749b8b62
Yangyu Chen <chen_yy@outlook.com>
2018-08-31 12:56:09 +0800

    Update drive.sh

--- drive.sh
+++ drive.sh
@@ -1,2 +1,4 @@
-python ./home/Sifo/Desktop/cookpw.py SIFO123 >password.bin
-sudo sg_raw -s 40 -i password.bin /dev/SDD c1 e1 00 00 00 00 00 00 28 00
+#!/bin/sh
+python cookpw.py $1 > /tmp/wd-password.bin
+sudo sg_raw -s 40 -i /tmp/wd-password.bin $2 c1 e1 00 00 00 00 00 00 28 00
+rm /tmp/wd-password.bin
